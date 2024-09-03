94d61aaca82a2b18a1455670d5c5e2223c2e0c5e
Mopey28 <160743314+Mopey28@users.noreply.github.com>
2024-09-04 02:33:26 +0800

    web.php

--- web.php
+++ web.php
@@ -16,3 +16,5 @@ Route::post('/products', [ProductController::class, 'store'])->name('products.st
 Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
 Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
